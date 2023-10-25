1dca0a9d8c2100606f0c0843478b0c7344f3224e
youssef <ll5575982>
2023-10-25 20:53:54 +0200

    modification

--- js/aceuill.js
+++ js/aceuill.js
@@ -41,35 +41,35 @@ function hideMenu(){
 
 
 
-function validationForm() {
-  var fullname = document.getElementById("fullname").value;
-  var email = document.getElementById("email").value;
-  var cin = document.getElementById("cin").value;
-  var num = document.getElementById("num").value;
+// function validationForm() {
+//   var fullname = document.getElementById("fullname").value;
+//   var email = document.getElementById("email").value;
+//   var cin = document.getElementById("cin").value;
+//   var num = document.getElementById("num").value;
 
-  var fullnameRegx = /^[a-zA-Z ]+$/;
-    const emailRegx = /^[a-zA-Z0-9]+@[a-z]+.com$/;
-    const cinRegx = /^[A-Z]{2}[0-9]{5}$/;
-    const numRegx = /^[0-9]{10}/;
+//   var fullnameRegx = /^[a-zA-Z ]+$/;
+//     const emailRegx = /^[a-zA-Z0-9]+@[a-z]+.com$/;
+//     const cinRegx = /^[A-Z]{2}[0-9]{5}$/;
+//     const numRegx = /^[0-9]{10}/;
 
-    if (!fullname.match(fullnameRegx)) {
-      alert("Nom incorrect");
-      return false;
-    }
-    if (!email.match(emailRegx)) {
-      alert("Email incorrect");
-      return false;
-    }
-    if (!cin.match(cinRegx)) {
-      alert("CIN incorrect");
-      return false;
-    }
-    if (!num.match(numRegx)) {
-      alert("Numéro de téléphone incorrect");
-      return false;
-    }
-    return true;
-  }
+//     if (!fullname.match(fullnameRegx)) {
+//       alert("Nom incorrect");
+//       return false;
+//     }
+//     if (!email.match(emailRegx)) {
+//       alert("Email incorrect");
+//       return false;
+//     }
+//     if (!cin.match(cinRegx)) {
+//       alert("CIN incorrect");
+//       return false;
+//     }
+//     if (!num.match(numRegx)) {
+//       alert("Numéro de téléphone incorrect");
+//       return false;
+//     }
+//     return true;
+//   }
 
 
 
